4fc83f97e3cc16bf32ace13a271590ec9773f7d8
Techori <harshrajsharma359@gmail.com>
2026-01-30 12:46:12 +0530

    Update ApiService.js

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,5 +1,5 @@
 // Last mein '/' lagana zaruri hai
-const API_URL = 'https://tiger2.onrender.com/'; 
+const API_URL = 'https://tiger2-1.onrender.com/'; 
 
 export const sendDataToServer = async (endpoint, payload) => {
   try {
